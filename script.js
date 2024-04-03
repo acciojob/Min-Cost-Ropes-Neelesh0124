7b4d244d64d103371d7a805cdc46a5b407ee3511
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-04-04 00:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function mincost(arr)
 //write your code here
 // return the min cost
 	let ans  = 0;
-	Arr.sort((a,b) => b-a);
+	arr.sort((a,b) => b-a);
    while (arr.length>1) {
    	let first = arr.pop();
 	let sec = arr.pop();
 	   ans += first + sec;
 	   arr.push(first + sec);
-	   Arr.sort((a,b) => b-a);
+	   arr.sort((a,b) => b-a);
 }
   return ans;
 }
